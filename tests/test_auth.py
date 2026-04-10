d4f09c961743f4821a53a13f3ce6f0faae44cfac
njobe <quandale.nottrippin@gmail.com>
2026-04-10 08:55:50 +0200

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -24,27 +24,19 @@ def test_register_invalid_email(driver):
         (By.CSS_SELECTOR, "[class*='error'], [class*='Error'], [role='alert']")))
     assert error is not None
 
-# TC_03 - Login validan
+# TC_03 - Login validan# TC_03 - Login validan (via cookies)
 def test_login_valid(driver):
-    driver.get("https://account.booking.com/sign-in")
+    driver.get("https://www.booking.com")
     wait = WebDriverWait(driver, 15)
-    email = wait.until(EC.presence_of_element_located((By.ID, "username")))
-    email.send_keys("vas_email@gmail.com")  # zamijeni
-    btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
-    driver.execute_script("arguments[0].click();", btn)
-    password = wait.until(EC.presence_of_element_located((By.ID, "password")))
-    password.send_keys("vasa_lozinka")  # zamijeni
-    btn2 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
-    driver.execute_script("arguments[0].click();", btn2)
+    # cookies već učitane u conftest.py, samo provjeri jesi li ulogovan
     wait.until(EC.url_contains("booking.com"))
     assert "sign-in" not in driver.current_url
-
 # TC_04 - Login pogrešan password
 def test_login_wrong_password(driver):
     driver.get("https://account.booking.com/sign-in")
     wait = WebDriverWait(driver, 15)
     email = wait.until(EC.presence_of_element_located((By.ID, "username")))
-    email.send_keys("vas_email@gmail.com")  # zamijeni
+    email.send_keys("b3njy34@gmail.com")  # zamijeni
     btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
     driver.execute_script("arguments[0].click();", btn)
     password = wait.until(EC.presence_of_element_located((By.ID, "password")))
